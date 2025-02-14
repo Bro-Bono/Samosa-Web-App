9ae66ff29f3a6825b74fa5a29d3b0061f8162943
aasgh <aasgh@Lenovosaurus>
2025-02-14 13:35:33 -0600

    [SAM-5]
    Implemented a 3-column dashboard with individual customer orders listed
    out in name and order details.

--- samosawebapp/src/main/java/com/brobono/samosawebapp/repositories/OrderRepository.java
+++ samosawebapp/src/main/java/com/brobono/samosawebapp/repositories/OrderRepository.java
@@ -1,10 +1,15 @@
 package com.brobono.samosawebapp.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.brobono.samosawebapp.models.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long>{
 	
-	//runs the following query: SELECT COUNT(*) FROM user WHERE status = ?
-	long countByStatus(String status); 
+	//runs the following query: SELECT COUNT(*) FROM orders WHERE status = ?
+	long countByStatus(String status);
+	
+	//runs the following query: SELECT * FROM orders WHERE status = ?;
+	List<Order> findByStatus(String status); 
 }
